cf83a47abb39e07c5707c1951940dafcb5733b2e
haple <aleph.oliveira@etec.sp.gov.br>
2020-06-15 08:56:37 -0300

    Challenge completed

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,54 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import path from 'path';
+import uploadConfig from '../config/upload';
 import Transaction from '../models/Transaction';
+import CreateTransactionService from './CreateTransactionService';
+
+async function loadCSV(filePath: string): Promise<any[]> {
+  const readCSVStream = fs.createReadStream(filePath);
+
+  const parseStream = csvParse({
+    from_line: 2,
+    ltrim: true,
+    rtrim: true,
+  });
+
+  const parseCSV = readCSVStream.pipe(parseStream);
+
+  const lines: any[] | PromiseLike<any[]> = [];
+
+  parseCSV.on('data', line => {
+    lines.push(line);
+  });
+
+  await new Promise(resolve => {
+    parseCSV.on('end', resolve);
+  });
+
+  return lines;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(fileName: string): Promise<Transaction[]> {
+    const csvFilePath = path.resolve(uploadConfig.directory, fileName);
+    const lines = await loadCSV(csvFilePath);
+
+    const createTransaction = new CreateTransactionService();
+
+    const transactions: Transaction[] = [];
+
+    for await (const line of lines) {
+      const transaction = await createTransaction.execute({
+        title: line[0],
+        type: line[1],
+        value: line[2],
+        category: line[3],
+      });
+      transactions.push(transaction);
+    }
+
+    return transactions;
   }
 }
 
